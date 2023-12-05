0f1cb0d1922d257b7b788bccdf99186aaf082a9d
Alif <79266506+S-Alif@users.noreply.github.com>
2023-12-05 11:36:18 +0600

    update

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -3,7 +3,8 @@ const { createToken, verifyToken } = require("../util/jwt");
 exports.cookieMaker = (tokenPayload) => {
   let cookieOption = {
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    httpOnly: false
+    httpOnly: false,
+    secure: true,
   }
 
   return { token: createToken(tokenPayload), cookieOption }
